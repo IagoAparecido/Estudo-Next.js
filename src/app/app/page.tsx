7f4c5b1c76054f176b76459cda33169a14c29e89
Iago <100394244+IagoAparecido@users.noreply.github.com>
2023-05-30 10:12:39 -0400

    first commit

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
-      <Link href="/dashboard">Dashboard</Link>
+      <h1>Dashboard</h1>
+      <Link href="/">Home</Link>
     </div>
   );
 }
---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+import Head from "next/head";
+
+export default function head() {
+  return (
+    <Head>
+      <title>Estudo Next.JS</title>
+    </Head>
+  );
+}
